ba061d28df9453da0c0e0dac93f377e7eb3ca067
olitreadwell <olitreadwell@gmail.com>
2016-02-17 16:27:05 -0600

    commas and such

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MarketPlaceApi::Application.routes.draw do
   #API definition
   namespace :api, defaults: { format: :json },
             constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1,
       constraints: ApiConstraints.new(version: 1, default: true) do
     #Where we are going to list our resources
     end
