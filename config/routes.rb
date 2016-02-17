cd6ade1aac19ede22b81118dab875755e9d5bd21
olitreadwell <olitreadwell@gmail.com>
2016-02-16 21:49:05 -0600

    Set api constraints and subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MarketPlaceApi::Application.routes.draw do
   #API definition
-  namespace :api, defaults { format: :json } do
+  namespace :api, defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/' do
     #Where we are going to list our resources
   end
 end
