3e9d44ff1824e3bf15c9161e164d2cb82e73b461
olitreadwell <olitreadwell@gmail.com>
2016-03-08 15:52:50 -0600

    remove unused portions of user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,4 @@ describe "when email is not present" do
   it { should validate_uniqueness_of(:email) }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
-  # before { @user.email = " " }
-  # it { should_not be_valid }
 end
